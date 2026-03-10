11a42c15d67ae6914513a475ad6b798c51931f21
Algot Johansson <algjo562@student.liu.se>
2026-03-10 11:45:37 +0100

    Add playback speed functionality

--- src/tak_simulator/time_keeper.py
+++ src/tak_simulator/time_keeper.py
@@ -5,15 +5,21 @@ class TimeKeeper:
     _real_time_at_last_unpause: float
     _fake_time_at_last_pause: float
     _is_paused: bool
+    _playback_speed: float
 
     def __init__(self):
         self._real_time_at_last_unpause = 0
         self._fake_time_at_last_pause = 0
         self._is_paused = True
+        self._playback_speed = 1.0
 
     @property
     def is_paused(self) -> bool:
         return self._is_paused
+
+    @property
+    def playback_speed(self) -> float:
+        return self._playback_speed
 
     def get_time(self) -> float:
         if self.is_paused:
@@ -21,8 +27,7 @@ class TimeKeeper:
 
         return (
             self._fake_time_at_last_pause
-            + time.time()
-            - self._real_time_at_last_unpause
+            + (time.time() - self._real_time_at_last_unpause) * self.playback_speed
         )
 
     def unpause(self):
@@ -32,3 +37,8 @@ class TimeKeeper:
     def pause(self):
         self._fake_time_at_last_pause = self.get_time()
         self._is_paused = True
+
+    def set_playback_speed(self, speed: float):
+        self._fake_time_at_last_pause = self.get_time()
+        self._real_time_at_last_unpause = time.time()
+        self._playback_speed = speed
