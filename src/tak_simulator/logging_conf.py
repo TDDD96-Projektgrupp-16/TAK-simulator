e6cf3793e1d5e10cfa0446db63234e5c95b6eb17
Arvid Rämsberg <arvra591@student.liu.se>
2026-04-16 17:37:18 +0200

    general mvc structure

--- src/tak_simulator/logging_conf.py
+++ src/tak_simulator/logging_conf.py
@@ -5,7 +5,7 @@ import logging
 def logging_setup(level):
     _LOGGING_CONFIG = {
         "version": 1,
-        "disable_existing_loggers": True,
+        "disable_existing_loggers": False,
         "formatters": {
             "standard": {"format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s"},
         },
