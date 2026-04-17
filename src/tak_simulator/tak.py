3a19765fe98b0bbab7869fd16613e21286c0a316
Arvid Rämsberg <arvra591@student.liu.se>
2026-04-17 07:27:50 +0200

    Formats code

--- src/tak_simulator/tak.py
+++ src/tak_simulator/tak.py
@@ -1,35 +1,34 @@
 import asyncio
 import threading
-import time
 
 from tak_simulator.scenario import load_scenario
 from tak_simulator.util import host_ip
 from tak_simulator.simulator import Simulator
+
 
 def _start_background_loop(loop: asyncio.AbstractEventLoop):
     """Helper to run the event loop in a background thread."""
     asyncio.set_event_loop(loop)
     loop.run_forever()
 
+
 class TAK:
-    def __init__(self, filename = None):
-        
+    def __init__(self, filename=None):
+
         self.simulator = None
         self.filename = filename
 
         self.loop = asyncio.new_event_loop()
-        threading.Thread(target=_start_background_loop, args=(self.loop,), daemon=True).start()
-
+        threading.Thread(
+            target=_start_background_loop, args=(self.loop,), daemon=True
+        ).start()
 
     def start(self):
-        if self.filename is None: 
+        if self.filename is None:
             return
         scenario = load_scenario(self.filename)
 
         host = host_ip()
         self.simulator = Simulator()
 
-        asyncio.run_coroutine_threadsafe(
-            self.simulator.run(scenario, host), 
-            self.loop
-        )
+        asyncio.run_coroutine_threadsafe(self.simulator.run(scenario, host), self.loop)
