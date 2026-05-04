26b0682e5c64a2a3473eee82f3fabcaa7a58b970
Arvid Rämsberg <arvra591@student.liu.se>
2026-05-04 15:16:20 +0200

    Revert "adds view for list view"
    This reverts commit 9562eea60eb1ea4c7a37b71700fc8c4af6d2ac5d.

--- src/tak_simulator/ui/list_view.py
+++
@@ -1,6 +0,0 @@
-from textual.app import ComposeResult
-from textual.widgets import DataTable, TabPane
-
-class EmulatorListMode(TabPane):
-    def compose(self) -> ComposeResult:
-        yield DataTable(id="emulator_table")
