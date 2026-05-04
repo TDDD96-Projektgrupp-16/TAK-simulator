9562eea60eb1ea4c7a37b71700fc8c4af6d2ac5d
arvra591 <arvra591@cyd-208.ad.liu.se>
2026-05-04 13:06:12 +0200

    adds view for list view

---
+++ src/tak_simulator/ui/list_view.py
@@ -0,0 +1,6 @@
+from textual.app import ComposeResult
+from textual.widgets import DataTable, TabPane
+
+class EmulatorListMode(TabPane):
+    def compose(self) -> ComposeResult:
+        yield DataTable(id="emulator_table")
